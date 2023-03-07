3479d7360706231c5b7e69e4fe796f8684635d23
Yurii Lunha <yurii.lunha@stansassets.com>
2023-03-07 16:15:49 +0100

    feat: added Name convention preview

---
+++ Editor/Core/NameConventionBuilder.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.RegularExpressions;
+
+namespace StansAssets.PackageManager
+{
+    static class NameConventionBuilder
+    {
+        internal static string BuildName(string name, NamingConvention convention)
+        {
+            var result = FormatTextByConvention(name, convention.ConventionType);
+            return $"{convention.Prefix}.{result}.{convention.Postfix}";
+        }
+
+        static string FormatTextByConvention(string text, NameConventionType convention)
+        {
+            switch (convention)
+            {
+                case NameConventionType.CamelCase:
+                {
+                    var output = Regex
+                        .Replace(text, @"\b(\w)", match => match.Value.ToUpper())
+                        .Replace(" ", "");
+                    
+                    return char.ToLower(output[0]) + output.Substring(1);
+                }
+                case NameConventionType.SnakeCase:
+                {
+                    var output = Regex.Replace(text, @"(\p{Ll})(\p{Lu})|(\W)+", "$1_$2");
+                    return output.ToLower();
+                }
+                case NameConventionType.KebabkCase:
+                {
+                    return Regex.Replace(text, @"(\p{Ll})(\p{Lu})|(\W)+", "$1-$2").ToLower();
+                }
+                case NameConventionType.PascalCase:
+                {
+                    var output = Regex.Replace(text, @"(?:^|\s)(\w)", match => match.Value.ToUpper());
+                    return output.Replace(" ", "");
+                }
+                case NameConventionType.None:
+                default:
+                {
+                    return text;
+                }
+            }
+        }
+    }
+}
--- Editor/UIToolkit/Tabs/Configuration/ConfigurationTab.cs
+++ Editor/UIToolkit/Tabs/Configuration/ConfigurationTab.cs
@@ -11,24 +11,50 @@ namespace StansAssets.PackageManager.Editor
             : base($"{PackageManagerConfig.WindowTabsPath}/Configuration/ConfigurationTab")
         {
             var conf = PackConfigurationSettings.Instance.ActiveConfiguration;
-            
+
             _ = new PackageTemplate(Root, conf);
             BindNamingConvention(Root, conf.NamingConvention);
+
+            UpdatePreview(Root, conf.NamingConvention);
         }
 
         static void BindNamingConvention(VisualElement root, NamingConvention namingConvention)
         {
             var prefix = root.Q<TextField>("prefix");
             prefix.SetValueWithoutNotify(namingConvention.Prefix);
-            prefix.RegisterValueChangedCallback(v => namingConvention.Prefix = v.newValue);
+            prefix.RegisterValueChangedCallback(v =>
+            {
+                namingConvention.Prefix = v.newValue;
+                UpdatePreview(root, namingConvention);
+            });
 
             var postfix = root.Q<TextField>("postfix");
             postfix.SetValueWithoutNotify(namingConvention.Postfix);
-            postfix.RegisterValueChangedCallback(v => namingConvention.Postfix = v.newValue);
+            postfix.RegisterValueChangedCallback(v =>
+            {
+                namingConvention.Postfix = v.newValue;
+                UpdatePreview(root, namingConvention);
+            });
 
             var nameConvention = root.Q<EnumField>("name-convention");
             nameConvention.SetValueWithoutNotify(namingConvention.ConventionType);
-            nameConvention.RegisterValueChangedCallback(v => namingConvention.ConventionType = (NameConventionType)v.newValue);
+            nameConvention.RegisterValueChangedCallback(v =>
+            {
+                namingConvention.ConventionType = (NameConventionType)v.newValue;
+                UpdatePreview(root, namingConvention);
+            });
+
+            var namingPreview = root.Q<TextField>("naming-preview-value");
+            namingPreview.RegisterValueChangedCallback(evt => UpdatePreview(root, namingConvention));
+        }
+
+        static void UpdatePreview(VisualElement root, NamingConvention namingConvention)
+        {
+            var namingPreview = root.Q<TextField>("naming-preview");
+            var namingPreviewValue = root.Q<TextField>("naming-preview-value");
+
+            var name = NameConventionBuilder.BuildName(namingPreviewValue.value, namingConvention);
+            namingPreview.SetValueWithoutNotify(name);
         }
     }
 }
