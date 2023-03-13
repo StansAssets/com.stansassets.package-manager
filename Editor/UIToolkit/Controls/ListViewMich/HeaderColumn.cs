db981e6882534c9ee057dca90e30eb3667a2b3d2
Yurii Lunha <yurii.lunha@stansassets.com>
2023-03-13 16:13:55 +0100

    style: added header in ListViewMich

---
+++ Editor/UIToolkit/Controls/ListViewMich/HeaderColumn.cs
@@ -0,0 +1,34 @@
+﻿#if UNITY_2019_4_OR_NEWER
+using UnityEngine.UIElements;
+
+namespace StansAssets.PackageManager.Editor
+{
+    class HeaderColumn
+    {
+        internal VisualElement Element { get; }
+
+        /// <summary>
+        /// Header column of <see cref="ListViewMich"/>
+        /// </summary>
+        /// <param name="text">text value</param>
+        /// <param name="space">Space in %..100%</param>
+        internal HeaderColumn(string text, float space = -1f)
+        {
+            if (space > 100f)
+            {
+                space = 100f;
+            }
+
+            Element = new Label(text)
+            {
+                style =
+                {
+                    flexShrink = 1f,
+                    flexGrow = 1f,
+                    width = new StyleLength(new Length(space, LengthUnit.Percent)),
+                }
+            };
+        }
+    }
+}
+#endif
--- Editor/UIToolkit/Controls/ListViewMich/ListViewMich.cs
+++ Editor/UIToolkit/Controls/ListViewMich/ListViewMich.cs
@@ -17,11 +17,38 @@ namespace StansAssets.PackageManager.Editor
         {
             UIToolkitEditorUtility.CloneTreeAndApplyStyle(this,
                 $"{PackageManagerConfig.ControlsPath}/ListViewMich/ListViewMich");
+
+            Header.style.visibility = Visibility.Hidden;
+        }
+
+        internal void InitHeader(string header)
+        {
+            Header.Add(new HeaderColumn(header).Element);
+            Header.style.visibility = Visibility.Visible;
+        }
+
+        internal void InitHeader(string[] headerColumns)
+        {
+            foreach (var column in headerColumns)
+            {
+                Header.Add(new HeaderColumn(column).Element);
+            }
+            Header.style.visibility = Visibility.Visible;
+        }
+
+        internal void InitHeader(HeaderColumn[] headerColumns)
+        {
+            foreach (var column in headerColumns)
+            {
+                Header.Add(column.Element);
+            }
+            Header.style.visibility = Visibility.Visible;
         }
 
         internal ListView ListView => this.Q<ListView>();
         internal Button AddButton => this.Q<Button>("add-item");
         internal Button RemoveButton => this.Q<Button>("remove-item");
+        internal VisualElement Header => this.Q<VisualElement>("list-header");
     }
 }
 
