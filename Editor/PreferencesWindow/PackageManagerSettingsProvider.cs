785cd6ac49ae3c77c6362c951b67a6996dc8ef25
Yurii Lunha <yurii_lunga@icloud.com>
2023-07-28 17:18:14 +0200

    refactoring: updated package manager preferences window

--- Editor/PreferencesWindow/PackageManagerSettingsProvider.cs
+++ Editor/PreferencesWindow/PackageManagerSettingsProvider.cs
@@ -1,74 +1,31 @@
 ﻿#if UNITY_2019_4_OR_NEWER
 
-using System.Collections.Generic;
+using JetBrains.Annotations;
 using StansAssets.Foundation.Editor;
 using StansAssets.Plugins.Editor;
 using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 
 namespace StansAssets.PackageManager.Editor
 {
-    class PackageManagerSettingsProvider : SettingsProvider
+    [UsedImplicitly]
+    class PackageManagerSettingsProvider : PackagePreferencesWindow
     {
-        readonly string m_WindowUIFilesRootPath = $"{PluginsDevKitPackage.UIToolkitPath}/SettingsWindow";
-
-        TabControl m_TabControl;
-
-        PackageManagerSettingsProvider(string path, SettingsScope scopes, IEnumerable<string> keywords = null)
-            : base(path, scopes, keywords)
+        protected override PackageInfo GetPackageInfo()
+            => PackageManagerUtility.GetPackageInfo(PackageManagerConfig.PackageName);
+        protected override string DisplayName => PackageManagerConfig.DisplayName;
+        protected override string SettingsPath => $"{PluginsDevKitPackage.RootMenu}/{DisplayName}";
+        protected override SettingsScope Scope => SettingsScope.Project;
+        protected override void OnActivate(string searchContext, VisualElement rootElement)
         {
+            ContentContainerFlexGrow(1);
+
+            AddTab("Manage", new ManagerTab());
+            AddTab("Configuration", new ConfigurationTab());
+            AddTab("About", new AboutTab());
         }
-
-        public override void OnActivate(string searchContext, VisualElement rootElement)
-        {
-            UIToolkitEditorUtility.CloneTreeAndApplyStyle(rootElement,
-                $"{m_WindowUIFilesRootPath}/PackageSettingsWindow");
-
-            // Hide our search bar. In preferences we already have search bar
-            // and it's value in "searchContext" parameter
-            var searchBar = rootElement.Q<ToolbarSearchField>();
-            if (searchBar != null)
-            {
-                searchBar.style.visibility = Visibility.Hidden;
-            }
-
-            var packageInfo = PackageManagerUtility.GetPackageInfo(PackageManagerConfig.PackageName);
-            rootElement.Q<Label>("display-name").text = packageInfo.displayName.Remove(0, "Stans Assets - ".Length);
-            rootElement.Q<Label>("description").text = packageInfo.description;
-            rootElement.Q<Label>("version").text = $"Version: {packageInfo.version}";
-
-            m_TabControl = new TabControl(rootElement);
-            m_TabControl.ContentContainerFlexGrow(1);
-            
-            m_TabControl.AddTab("manager", "Manage", new ManagerTab());
-            m_TabControl.AddTab("configuration", "Configuration", new ConfigurationTab());
-            m_TabControl.AddTab("about", "About", new AboutTab());
-
-            m_TabControl.ActivateTab("configuration");
-        }
-
-        public override void OnDeactivate()
-        {
-            base.OnDeactivate();
-            
-            PackConfigurationSettings.Save();
-            PackManagerAssetSettings.Save();
-        }
-
-        [SettingsProvider]
-        static SettingsProvider RegisterSettingsProvider()
-        {
-            var provider = new PackageManagerSettingsProvider(
-                PackageManagerConfig.RootMenu,
-                SettingsScope.Project,
-                new[] { "Plugin", "Manager", "Package", "Stan", "Assets", "Tool" })
-            {
-                label = PackageManagerConfig.DisplayName
-            };
-
-            return provider;
-        }
+        protected override void OnDeactivate() { }
     }
 }
 
